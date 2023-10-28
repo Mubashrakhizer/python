1e48b42cbb5b6a6cdf118b7e538b264d1bc8d1e1
Mubashrakhizer <146741888+Mubashrakhizer@users.noreply.github.com>
2023-10-28 19:47:43 +0500

    Add files via upload
    This is a simple python program to calculate grade

---
+++ Grade calculator.py
@@ -0,0 +1,14 @@
+
+marks = int(input("Enter the marks for grade : "))
+if 80 <= marks <= 100 :
+  print("You got A grade")
+if 70 <= marks <= 80 :
+  print("You got B grade")
+if 60 <= marks <= 70 :
+  print("You got C grade")
+if 50 <= marks <= 60 :
+  print("You got D grade")
+if 40 <= marks <= 50 :
+  print("You got E grade")
+if 0 <= marks <= 40 :
+  print("You are failed")
